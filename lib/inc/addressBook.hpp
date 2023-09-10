432150d4f8c2422e95fa0f0e0ab10afa4bcdd4e8
Michael Augaitis <michael.aug@proton.me>
2023-09-10 15:47:13 +0100

    Make RetrieveContactsSortedByLastName test pass

--- lib/inc/addressBook.hpp
+++ lib/inc/addressBook.hpp
@@ -1,18 +1,21 @@
 #pragma once
 #include "contact.hpp"
-#include <vector>
-#include <unordered_set>
 #include <map>
 #include <memory>
+#include <unordered_set>
+#include <vector>
 
 typedef std::vector<std::shared_ptr<Contact>> contactPointers;
 typedef std::map<std::string, std::weak_ptr<Contact>> contactMapping;
 
 class AddressBook {
 public:
-    void addContact(Contact);
-    std::vector<Contact> contactsByFirstName();
+  // Take Contacts by value because it's safest
+  void addContact(Contact);
+  std::vector<Contact> contactsByFirstName();
+  std::vector<Contact> contactsByLastName();
 private:
-    contactPointers entries; // Store entries directly
-    contactMapping by_first_last; // Map for sorting by first name + last name
+  contactPointers entries;      // Store entries directly, owns the pointers
+  contactMapping by_first_last; // Map for sorting by first name + last name
+  contactMapping by_last_first; // Map for sorting by last name + first name
 };
--- lib/src/addressBook.cpp
+++ lib/src/addressBook.cpp
@@ -1,19 +1,29 @@
 #include "addressBook.hpp"
 #include <memory>
+#include <vector>
+
+static std::vector<Contact> buildVecFromMap(const contactMapping &map) {
+  std::vector<Contact> contacts;
+  for (const auto &pair : map) {
+
+    if (auto shared_contact = pair.second.lock()) {
+      contacts.push_back(*shared_contact);
+    }
+  }
+  return contacts;
+}
 
 void AddressBook::addContact(Contact contact) {
-    auto contactPtr = std::make_shared<Contact>(contact);
-    entries.push_back(contactPtr);
-    by_first_last[contactPtr->firstName + contactPtr -> lastName] = contactPtr;
+  auto contactPtr = std::make_shared<Contact>(contact);
+  entries.push_back(contactPtr);
+  by_first_last[contactPtr->firstName + contactPtr->lastName] = contactPtr;
+  by_last_first[contactPtr->lastName + contactPtr->firstName] = contactPtr;
 }
 
 std::vector<Contact> AddressBook::contactsByFirstName() {
-    std::vector<Contact> contacts;
-    for(const auto& pair : by_first_last) {
+  return buildVecFromMap(by_first_last);
+}
 
-        if(auto shared_contact = pair.second.lock()) {
-            contacts.push_back(*shared_contact);
-        }
-    }
-    return contacts;
+std::vector<Contact> AddressBook::contactsByLastName() {
+  return buildVecFromMap(by_last_first);
 }
